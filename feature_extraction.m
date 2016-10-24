de10fb09e3eb24cf035b72f5fda998b40109b9a3
Alex-Lee06 <uberman06@gmail.com>
2016-10-24 13:13:49 -0400

    extraction updated

--- feature_extraction.m
+++ feature_extraction.m
@@ -4,12 +4,49 @@ function feat = feature_extraction(img)
 % Output should be a fixed length vector [1*dimension] for a single image. 
 % Please do NOT change the interface.
 
-%%Read image, turn it gray and extract points.
-img = imageDatastore(fullfile('feature_extraction-master', {'train', 'val'}), 'IncludeSubfolders', true, 'LabelSource', 'foldernames', 'FileExtensions', '.jpg');
-img.Labels
+%%Read image and detect feature points.
+imgData = imageDatastore(fullfile('val'), 'IncludeSubfolders', true, 'LabelSource', 'foldernames', 'FileExtensions', '.jpg');
+%[trainingSets, testSets] = splitEachLabel(img, 60);
+for i = 1:length(imgData.Files)
+    data = readimage(imgData,i);
+    grayData = rgb2gray(data);
+    feat = detectSURFFeatures(grayData);
+    [f1, vpts1] = extractFeatures(grayData, feat);
+    
+    
+    idx3 = kmeans(X,3,'Distance', vpts1);
+    %crop image for visual words
+    for p = 1:length(vpts1)
+        vWords = imcrop(grayData, vpts1);
+    end
+end
 
 
-[trainingSets, testSets] = splitEachLabel(img, 1);
+
+
+
+%%feature is working.
+
+%grayImage = rgb2gray(img);
+
+%scale = 1.3;
+%J = imresize(grayImage, scale);
+%theta = 31;
+%distorted = imrotate(J, theta);
+%figure
+%imshow(distorted);
+
+
+%%Extract features
+%[f1,vpts1] = extractFeatures(grayImg1, feat);
+
+
+
+%imageRead = readimage(img, 1);
+%grayImage = rgb2gray(imageRead);
+%imshow(grayImage);
+%detect feature points
+
 
 
 
